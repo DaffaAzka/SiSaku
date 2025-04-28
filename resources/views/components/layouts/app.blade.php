1fdcbbb736a891d5536ddfc08091e66295384d24
Daffa Azka <azkadaiki26@gmail.com>
2025-04-28 12:38:45 +0700

    add fonts

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sisaku</title>
+
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 
+    <style>
+        .font-sagoe {
+            font-family: 'Sagoe UI';
+        }
+    </style>
+
 </head>
+
 <body class="bg-neutral-50 dark:bg-neutral-900">
 
-    {{-- @if (!request()->is('login') && !request()->is('register')  && !request()->is('/') && !request()->is('register')  && !request()->is('verify')) --}}
+    {{-- @if (!request()->is('login') && !request()->is('register') && !request()->is('/') && !request()->is('register') && !request()->is('verify')) --}}
     <livewire:navbar />
     {{-- @endif --}}
 
@@ -18,4 +27,5 @@
     {{ $slot }}
 
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,10 @@
 <x-layouts.app>
 
-    
+
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+
+        
+    </div>
+
+
 </x-layouts.app>
