c8332907ce1b68680212d3a3d688e31132ac734e
Satria <satriahutama050@gmail.com>
2025-04-29 07:10:55 +0700

    responsive verify

--- resources/views/livewire/auth/verify.blade.php
+++ resources/views/livewire/auth/verify.blade.php
@@ -1,28 +1,28 @@
-<div class="min-h-screen bg-teal-500 flex items-center justify-center">
+<div class="min-h-screen bg-teal-500 flex items-center justify-center p-4">
     <div class="flex bg-white rounded-lg shadow-lg overflow-hidden max-w-2xl w-full">
 
         <!-- Form Verifikasi -->
-        <div class="w-1/2 p-8 flex flex-col justify-center">
-        <h2 class="text-4xl font-black text-black mb-6 font-sagoe">Verifikasi Email</h2>
-        <p class="text-sm text-gray-600 mb-4">Kode telah dikirimkan ke perangkat anda, silahkan cek inbox email</p>
+        <div class="sm:w-1/2 p-8 flex flex-col justify-center">
+        <h2 class="text-2xl sm:text-4xl font-black text-black mb-6 font-sagoe">Verifikasi Email</h2>
+        <p class="text-xs sm:text-sm text-gray-600 mb-4">Kode telah dikirimkan ke perangkat anda, silahkan cek inbox email</p>
 
         <form action="#" method="POST" class="space-y-4">
             <div class="flex items-center border rounded px-3 py-2 space-x-2">
                 <x-lucide-mail class="w-3.5  h-3.5"/>
                 <input type="text" name="code" placeholder="" class="w-full focus:outline-none">
             </div>
-            <button type="submit" class="w-full bg-teal-700 hover:bg-teal-500 text-white font-semibold py-2 px-4 rounded">
+            <button type="submit" class="w-full bg-teal-700 hover:bg-teal-500 text-white font-semibold py-2 px-4 rounded text-sm">
                 Submit
             </button>
         </form>
-        <p class="text-center text-sm text-gray-500 mt-4">
+        <p class="text-center text-xs text-gray-500 mt-4">
             Belum menerima kode?
             <a href="#" class="text-teal-700 font-semibold hover:underline">Kirim Ulang</a>
          </p>
      </div>
 
         <!-- Gambar -->
-            <div class="w-1/2">
+            <div class="hidden sm:block sm:w-1/2">
              <img src="{{ asset('assets/verify.jpg') }}" alt="Verifikasi" class="object-cover w-full h-full">
             </div>
         </div>
