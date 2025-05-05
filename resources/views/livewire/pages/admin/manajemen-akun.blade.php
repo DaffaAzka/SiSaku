0950e73944ba3d54287ae04f03a782467ed48c09
Satria <satriahutama050@gmail.com>
2025-05-05 12:40:57 +0700

    benerin manajemen akun

--- resources/views/livewire/pages/admin/manajemen-akun.blade.php
+++ resources/views/livewire/pages/admin/manajemen-akun.blade.php
@@ -18,7 +18,7 @@
       <div class="col-span-1">
         <select
           class="border border-gray-300 rounded-lg px-4 py-2 text-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500">
-          <option selected disabled>Kelas</option>
+          <option selected disabled>Role</option>
         </select>
       </div>
 
