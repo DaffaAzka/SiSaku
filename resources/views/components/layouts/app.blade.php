d300fb382c4e3057e6ebfad1501627f36bd88c92
Daffa Azka <azkadaiki26@gmail.com>
2025-05-18 16:41:57 +0700

    deployment

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,11 +12,12 @@
 
     <link rel="ico" href="{{ asset('logo_transparant.svg') }}" type="image/x-icon">
 
-    <link rel="stylesheet" href="{{ asset('build/assets/app-DC9SDumM.css') }}">
-    <script src="{{ asset('build/assets/app-CZvt5OcE.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('app-DC9SDumM.css') }}">
+    <script src="{{ asset('app-CZvt5OcE.js') }}"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
